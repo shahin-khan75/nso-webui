46e3cb71fd6ef67817ea1da07b5ca51e1e109d83
mahesh <mahesh.kumar@infinitylabs.in>
2017-03-29 14:21:57 +0530

    get update data

--- NsoWebUI/src/com/infinitylabs/nso/controller/NSOController.java
+++ NsoWebUI/src/com/infinitylabs/nso/controller/NSOController.java
@@ -1,25 +1,18 @@
 package com.infinitylabs.nso.controller;
 
-import java.util.Arrays;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.infinitylabs.nso.bean.LoginBean;
@@ -36,11 +29,12 @@ public class NSOController {
     public ModelAndView loginPage() {  
         return new ModelAndView("login", "loginBean", loginBean);  
     }  
+	
 	@RequestMapping(value="/login",method = RequestMethod.POST)
 	public ModelAndView loginMethod(@ModelAttribute("loginBean") LoginBean loginBean,HttpServletRequest request,BindingResult result){
 		HttpSession session;
 		ResponseEntity<String> loginResponse = logicService.getLogin(loginBean);
-		System.out.println(loginResponse.getBody());
+		System.out.println("Response String  "+loginResponse.getBody());
 			if (loginResponse.getStatusCode() != HttpStatus.OK) {
 				throw new RuntimeException("Failed : HTTP error code : " + loginResponse.getStatusCode());
 			}
@@ -51,7 +45,7 @@ public class NSOController {
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			System.out.println(outputjson.toString());
+			System.out.println("Response Json :"+outputjson.toString());
 			if(outputjson.has("result")){
 				session=request.getSession(true);
 				session.setAttribute("user",request.getParameter("username"));
